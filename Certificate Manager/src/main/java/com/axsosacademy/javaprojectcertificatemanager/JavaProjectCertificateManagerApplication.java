02c80c33c1a1528283e868fac57ed685685a7801
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-12-31 16:09:43 +0200

    new

---
+++ Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/JavaProjectCertificateManagerApplication.java
@@ -0,0 +1,13 @@
+package com.axsosacademy.javaprojectcertificatemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectCertificateManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectCertificateManagerApplication.class, args);
+    }
+
+}
