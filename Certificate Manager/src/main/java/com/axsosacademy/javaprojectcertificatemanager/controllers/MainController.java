7da895251449c358f7e61c53965765961fa2c060
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-02 11:06:06 +0200

    login page

--- Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/controllers/MainController.java
+++ Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/controllers/MainController.java
@@ -1,19 +1,64 @@
 package com.axsosacademy.javaprojectcertificatemanager.controllers;
 
-
+import com.axsosacademy.javaprojectcertificatemanager.models.LoginUser;
+import com.axsosacademy.javaprojectcertificatemanager.models.User;
+import com.axsosacademy.javaprojectcertificatemanager.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
+    private final UserService userService;
+
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Login Page
     @GetMapping("/")
-    public String index() {
+    public String login(Model model) {
+        model.addAttribute("newLogin", new LoginUser());
         return "login";
     }
 
+    // Login Method
+    @PostMapping("/loginUser")
+    public String loginUser(
+            @Valid @ModelAttribute("newLogin") LoginUser newLogin,
+            BindingResult bindingResult,
+            HttpSession session
+    ) {
+        User loggedUser = userService.loginUser(newLogin, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "login";
+        }
+        else {
+            session.setAttribute("loggedUser", loggedUser);
+            Long departmentId = loggedUser.getDepartment().getId();
+            if (departmentId == 1) {
+                return "redirect:/adminDashboard";
+            } else if (departmentId == 2) {
+                return "redirect:/teacherDashboard";
+            } else if (departmentId == 3) {
+                return "redirect:/accountantDashboard";
+            }
+            return "redirect:/";
+        }
+    }
+
+
     @GetMapping("/adminDashboard")
-    public String dashboard() {
+    public String dashboard(HttpSession session) {
+        if (session.getAttribute("loggedUser") == null) {
+            return "redirect:/";
+        }
         return "adminDashboard";
     }
 
