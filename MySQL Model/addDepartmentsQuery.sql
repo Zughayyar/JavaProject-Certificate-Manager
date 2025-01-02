c198b0ce8271190620e75499402222defd3b59bf
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-02 11:55:43 +0200

    mysql

---
+++ MySQL Model/addDepartmentsQuery.sql
@@ -0,0 +1,8 @@
+USE `JavaProject_Cert_Manager`;
+SHOW CREATE TABLE departments;
+insert into departments (id,department_name,department_description) values (1,'Adminstration', 'Admin users can add and edit teachers and accountants');
+insert into departments (id,department_name,department_description) values (2,'Teachers Department', 'Teachers as part of the academy to approve grades for students');
+insert into departments (id,department_name,department_description) values (3,'Financial Department', 'Accountants as part of the Finacial to approve accounts for students');
+SELECT * FROM JavaProject_Cert_Manager.departments;
+
+SELECT * FROM JavaProject_Cert_Manager.users;
