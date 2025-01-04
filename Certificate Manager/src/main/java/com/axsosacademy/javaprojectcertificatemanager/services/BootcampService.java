50c423894c1f55c28b3224f8b9041634eafbc7a8
Sami <sami.daraghmeh1306@gmail.com>
2025-01-04 13:19:03 +0200

    add bootcamp details

--- Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/services/BootcampService.java
+++ Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/services/BootcampService.java
@@ -1,7 +1,11 @@
 package com.axsosacademy.javaprojectcertificatemanager.services;
 
 import com.axsosacademy.javaprojectcertificatemanager.models.Bootcamp;
+import com.axsosacademy.javaprojectcertificatemanager.models.Student;
 import com.axsosacademy.javaprojectcertificatemanager.repositories.BootcampRepository;
+import com.axsosacademy.javaprojectcertificatemanager.repositories.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +16,8 @@ public class BootcampService {
     public BootcampService(BootcampRepository bootcampRepository) {
         this.bootcampRepository = bootcampRepository;
     }
+      @Autowired
+    private StudentRepository studentRepository;
 
     // Get All Bootcamps
     public List<Bootcamp> getAllBootcamps() {
@@ -23,4 +29,11 @@ public class BootcampService {
         bootcampRepository.save(bootcamp);
     }
 
+    public Bootcamp getBootcamp(Long id) {
+        return bootcampRepository.findById(id).orElseThrow(() -> new RuntimeException("Bootcamp not found"));
+    }
+
+    public List<Student> getStudentsForBootcamp(Long bootcampId) {
+        return studentRepository.findByCertificates_Bootcamps_Id(bootcampId);  // Correct query method
+    }
 }
