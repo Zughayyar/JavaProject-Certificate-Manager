634165ad1ce42b25cbab7e76b2babe1e957c68aa
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-04 16:06:27 +0200

    teacher dashboard

---
+++ Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/controllers/TeacherController.java
@@ -0,0 +1,42 @@
+package com.axsosacademy.javaprojectcertificatemanager.controllers;
+
+import com.axsosacademy.javaprojectcertificatemanager.models.Bootcamp;
+import com.axsosacademy.javaprojectcertificatemanager.services.BootcampService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class TeacherController {
+
+    private final BootcampService bootcampService;
+
+    public TeacherController(BootcampService bootcampService) {
+        this.bootcampService = bootcampService;
+    }
+
+
+    @GetMapping("/teacherDashboard")
+    public String teacherDashboard(HttpSession session, Model model) {
+        if (session.getAttribute("loggedUser") == null) {
+            return "redirect:/";
+        }
+        List<Bootcamp> bootcamps = bootcampService.getAllBootcamps();
+        Collections.reverse(bootcamps);
+        model.addAttribute("bootcamps", bootcamps);
+        return "teacherDashboard";
+    }
+
+
+    @GetMapping("/teacher/bootcamp/{id}")
+    public String showCertificatesForBootcamp(@PathVariable("id") Long bootcampId, Model model) {
+        Bootcamp bootcamp = bootcampService.getBootcamp(bootcampId);
+        model.addAttribute("bootcamp", bootcamp);
+        return "certificates_teacher";  // Thymeleaf template name
+    }
+
+}
