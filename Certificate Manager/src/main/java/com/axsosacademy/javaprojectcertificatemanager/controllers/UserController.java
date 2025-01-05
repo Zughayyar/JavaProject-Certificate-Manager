0188723125b8e7ab669f7f1e107a871aee030d67
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-05 10:23:20 +0200

    new

---
+++ Certificate Manager/src/main/java/com/axsosacademy/javaprojectcertificatemanager/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.axsosacademy.javaprojectcertificatemanager.controllers;
+
+import com.axsosacademy.javaprojectcertificatemanager.models.User;
+import com.axsosacademy.javaprojectcertificatemanager.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/usersDetails")
+    public String usersDetails(Model model) {
+        List<User> users = userService.getAllUsersWithRoles();
+        List<Object[]> usersDetails = userService.getUsersAndRolesDetails();
+        model.addAttribute("users", users);
+        model.addAttribute("usersDetails", usersDetails);
+        return "usersDetails";
+    }
+}
